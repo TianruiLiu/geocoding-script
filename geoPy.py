dc30be1704de54bfef4f6d97e4d703ffd2dec452
TianruiLiu <tliu1@macalester.edu>
2020-11-27 22:24:26 -0600

    trial

---
+++ geoPy.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from geopy import Nominatim
+import io
+
+geolocator = Nominatim(user_agent="example app")
+
+df = pd.read_csv("/Users/liutianrui/Downloads/bpsData.csv")
+# print(df.head())
+
+df["loc"] = df["kecnm14"].apply(geolocator.geocode)
+df["point"]= df["loc"].apply(lambda loc: tuple(loc.point) if loc else None)
+
+df[['lat', 'lon', 'altitude']] = pd.DataFrame(df['point'].to_list(), index=df.index)
+print(df.head())
+
+
+
+
